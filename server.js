78854f1a45a92e58014c3465ff485c0d116ebc6f
Yuan <yuangao2.12@outlook.com>
2017-05-16 13:01:08 -0400

    Set dynamic port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 
 var app = express();
 
-var server = app.listen(3000, listening);
+var server = app.listen(process.env.PORT || 3000, listening);
 
 function listening() {
   console.log('listening at port 3000...');
